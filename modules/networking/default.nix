ca8b74e96166226c5e573b1aac4c8032a8fbc65c
akazdayo <82073147+akazdayo@users.noreply.github.com>
2026-01-04 02:59:52 +0900

    openssh

--- modules/networking/default.nix
+++ modules/networking/default.nix
@@ -7,4 +7,15 @@
     "1.0.0.1"
   ];
   services.nscd.enableNsncd = true;
+
+  # SSH
+  services.openssh = {
+    enable = true;
+    ports = [ 22 ];
+    settings = {
+      PermitRootLogin = "no";
+      PasswordAuthentication = false;
+      KbdInteractiveAuthentication = false;
+    };
+  };
 }
--- modules/users/default.nix
+++ modules/users/default.nix
@@ -1,8 +1,16 @@
-{pkgs, ...}: {
+{ pkgs, ... }:
+{
   users.users.akazdayo = {
     isNormalUser = true;
     description = "akazdayo";
-    extraGroups = ["networkmanager" "wheel" "docker"];
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+      "docker"
+    ];
     shell = pkgs.nushell;
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDrvifm9j0kjjoEUWf+QeFxQgdA9XPYc/VRyS9oPL+X5"
+    ];
   };
 }
