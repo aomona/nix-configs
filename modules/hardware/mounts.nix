6ea626bec3dafaefcf073624f019033abc98d594
akazdayo <82073147+akazdayo@users.noreply.github.com>
2026-04-02 20:31:08 +0900

    mountついかしたお

--- modules/hardware/mounts.nix
+++ modules/hardware/mounts.nix
@@ -1,16 +1,6 @@
 { pkgs, ... }:
 {
-  fileSystems."/mnt/Data" = {
-    device = "/dev/disk/by-uuid/B4B4B9ACB4B9718A";
-    fsType = "ntfs";
-    options = [
-      "rw"
-      "uid=1000"
-      "nofail"
-    ];
-  };
-
-  fileSystems."/mnt/sdd1" = {
+  fileSystems."/mnt/kioxia" = {
     device = "/dev/disk/by-uuid/7d2f187f-18cb-4c3b-8f5f-cccb8a337afc";
     fsType = "ext4";
     options = [
@@ -28,4 +18,19 @@
       "nofail"
     ];
   };
+
+  fileSystems."/mnt/vaio" = {
+    device = "/dev/disk/by-uuid/7AB6CF81B6CF3C7F";
+    fsType = "ntfs-3g";
+    options = [
+      "rw"
+      "uid=1000"
+      "nofail"
+    ];
+  };
+
+  # USB再接続時に自動でマウントする
+  services.udev.extraRules = ''
+    ACTION=="add", SUBSYSTEM=="block", ENV{ID_FS_UUID}=="7AB6CF81B6CF3C7F", TAG+="systemd", ENV{SYSTEMD_WANTS}="mnt-vaio.mount"
+  '';
 }
