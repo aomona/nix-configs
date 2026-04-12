d0284a671b20ed35640084926ebf382c2e973cd3
akazdayo <82073147+akazdayo@users.noreply.github.com>
2026-04-12 18:45:27 +0900

    データ永続化

--- modules/containers/immich.nix
+++ modules/containers/immich.nix
@@ -1,9 +1,38 @@
 { ... }:
+let
+  hostDataRoot = "/var/lib/immich-container";
+in
 {
+  systemd.tmpfiles.rules = [
+    "d ${hostDataRoot} 0755 root root -"
+    "d ${hostDataRoot}/media 0755 root root -"
+    "d ${hostDataRoot}/postgresql 0755 root root -"
+    "d ${hostDataRoot}/redis 0755 root root -"
+    "d ${hostDataRoot}/cache 0755 root root -"
+  ];
+
   containers.immich = {
     autoStart = true;
     privateNetwork = true;
     macvlans = [ "eno1" ];
+    bindMounts = {
+      "/var/lib/immich" = {
+        hostPath = "${hostDataRoot}/media";
+        isReadOnly = false;
+      };
+      "/var/lib/postgresql" = {
+        hostPath = "${hostDataRoot}/postgresql";
+        isReadOnly = false;
+      };
+      "/var/lib/redis-immich" = {
+        hostPath = "${hostDataRoot}/redis";
+        isReadOnly = false;
+      };
+      "/var/cache/immich" = {
+        hostPath = "${hostDataRoot}/cache";
+        isReadOnly = false;
+      };
+    };
 
     config =
       { ... }:
