fac023b68f3079f190463bfcb3248b5ff4ca2db4
akazdayo <82073147+akazdayo@users.noreply.github.com>
2026-05-05 03:03:26 +0900

    arpaを追加してみた

--- modules/containers/pihole-unbound.nix
+++ modules/containers/pihole-unbound.nix
@@ -51,6 +51,7 @@ in
           settings = {
             dns = {
               upstreams = [ "127.0.0.1#5335" ];
+              hosts = [ "192.168.11.63 nas.home.arpa" ];
               dnssec = true;
               bogusPriv = true;
               domainNeeded = true;
